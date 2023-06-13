3fd1617b8f8e707e59559945d22a48b81587df51
vikram <133756180+vikraman1653763@users.noreply.github.com>
2023-06-13 16:35:01 +0530

    Update wejs.js

--- wejs.js
+++ wejs.js
@@ -39,8 +39,7 @@ document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
 
-document
-  .querySelector(".search-bar").addEventListener("keyup", function (event) {
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
       weather.search();
     }
